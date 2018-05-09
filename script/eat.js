b188ed2e8334191e73599d6008d32aaa53d38aa2
seror <seror94ab@yahoo.com>
2018-05-09 11:12:20 -0500

    update

--- script/eat.js
+++ script/eat.js
@@ -11,7 +11,6 @@ $("#search-btn").on("click", function(event) {
   var queryURL =
     "https://developers.zomato.com/api/v2.1/cities?q=" + eatSearching;
 
-
   $.ajax({
     url: queryURL,
     method: "GET",
@@ -20,28 +19,28 @@ $("#search-btn").on("click", function(event) {
     }
   }).then(function(response) {
     for (i = 0; i < response.location_suggestions.length; i++) {
-<<<<<<< HEAD
       console.log(response.location_suggestions[i].state_name);
 
-=======
->>>>>>> 5d9da8df5c54fdaab0ffb4eef56d25959b1c6dee
       $("#holder").append(
-        "<p class = 'city'><button class = 'getCity' value = " + i+" >" +
-          JSON.stringify(response.location_suggestions[i].name).replace(/"/gi,"") +
+        "<p class = 'city'><button class = 'getCity' value = " +
+          i +
+          " >" +
+          JSON.stringify(response.location_suggestions[i].name).replace(
+            /"/gi,
+            ""
+          ) +
           " </button></p>"
       );
     }
   });
 });
 
-$("#holder").on("click", ".getCity",function(event){
-  
+$("#holder").on("click", ".getCity", function(event) {
   var cities = $("#cities-input").val();
   var position = $(this).val();
 
   var queryURL = "https://developers.zomato.com/api/v2.1/cities?q=" + cities;
 
- 
   $.ajax({
     url: queryURL,
     method: "GET",
@@ -50,8 +49,9 @@ $("#holder").on("click", ".getCity",function(event){
     }
   }).then(function(response) {
     info = response.location_suggestions[position];
-    $("#cities-input").replaceWith("<p>"+response.location_suggestions[position].name +"</p>")
+    $("#cities-input").replaceWith(
+      "<p>" + response.location_suggestions[position].name + "</p>"
+    );
     $(".city").remove();
-    
   });
 });
