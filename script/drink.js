081341409c2de2f088080ddc3562264ebd3ba8e5
ColePresnell <cole.presnell@gmail.com>
2018-05-14 22:31:47 -0500

    changed some of the result styling on drink, tried to get next and previous buttons working

--- script/drink.js
+++ script/drink.js
@@ -17,7 +17,27 @@ $(document).ready(function () {
       event.preventDefault();
 
       // Here we grab the text from the input box      
-      var barSearchInput = $("#zip-input").val();   
+      var barSearchInput = $("#zip-input").val(); 
+      //attempting to add "next", "previous" button functionality
+      var offset = 0;
+      var limit = 5;
+
+      $("#drinkNextButton").on("click", function (event) {
+        event.preventDefault();
+        offset + 5;
+        limit + 5;
+
+       // function (response);  
+    }); 
+
+    $("#drinkPreviousButton").on("click", function (event) {
+        event.preventDefault();
+        offset - 5;
+        limit - 5;
+
+       // $.get(proxyUrl + targetUrl, function (response));  
+    }); 
+      
       if ($("#inputGroupSelect01").val() && $("#inputGroupSelect01").val() !== 'none')
         barSearchInput += "&type=" + $("#inputGroupSelect01").val();
       
@@ -25,7 +45,7 @@ $(document).ready(function () {
           barSearchInput
      })
       // Here we construct our URL
-      var queryURL = "https://api.barzz.net/api/search?zip=" + barSearchInput + "&user_key=8dd53e25d52b2fc98fb6a563b70feaaf";
+      var queryURL = "https://api.barzz.net/api/search?zip=" + barSearchInput + "&offset=" + offset + "&limit=" + limit + "&user_key=8dd53e25d52b2fc98fb6a563b70feaaf";
       console.log(queryURL);
 
       var proxyUrl = 'https://cors-anywhere.herokuapp.com/';
@@ -35,7 +55,7 @@ $(document).ready(function () {
         console.log(response);
         var response1 = JSON.parse(response);
 
-        for (var i = 0; i < 5; i++) {
+        for (var i = offset; i < limit; i++) {
           console.log(response1.success.results[i]);
           var barTitle = response1.success.results[i].Name;
           var barImage = response1.success.results[i].Bar_Image;
@@ -46,10 +66,13 @@ $(document).ready(function () {
           var barType = response1.success.results[i].Type;
 
           $("#barCard").append(
-            "<div class='row bar-container'><div class='col-md-3'><img class='barPhoto'src='" + barImage + "'/></div>" + "<div class = 'col-md-9'><a href ='" +  barURL + "'target='_blank'>" + barTitle + "</a><br/>" + "Address: " + barAddress + "<br/>" + "Website: "  + barWebsite + "<br/>" + "Hours: " + barHours + "<br/>" + "Venue Type: " + barType + "</div></div>")
+            "<div class='row bar-container'><div class='col-md-3'><img class='barPhoto'src='" + barImage + "'/></div>" + "<div class = 'col-md-9'><a id=barTitle href ='" +  barURL + "'target='_blank'>" + barTitle + "</a><br/>" + "Address: " + barAddress + "<br/>" + "Website: "  + barWebsite + "<br/>" + "Hours: " + barHours + "<br/>" + "Venue Type: " + barType + "</div></div>")
         };
       });
     });
+
+// trying to add "next" and "previous" functionality
+
     firebase.database().ref().on("child_added",function(snapshot){
       var drink = (snapshot.val().barSearchInput);
       drinks.push(drink);
