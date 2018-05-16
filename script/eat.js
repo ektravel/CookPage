564473ce86bb71e28102feb6d4156c90dea61ede
Sam Llamas <sam.llamas@utexas.edu>
2018-05-16 17:05:56 -0500

    eat page update

--- script/eat.js
+++ script/eat.js
@@ -1,5 +1,5 @@
 // This .on("click") function will trigger the AJAX Call
-
+var start = 0
 var info = "";
 
 var cityInfo = false;
@@ -26,9 +26,12 @@ $("#search-btn").on("click", function (event) {
         $("#holder").append("<p class = 'city'><button class = 'getCity' value = " + i + " >" +
           JSON.stringify(response.location_suggestions[i].title).replace(/"/gi, "") + " </button></p>"
         );
+        
       }
     });
   } else if (cityInfo === true) {
+
+    
     event.preventDefault();
     // Here we grab the text from the input box
     var cuisine = $("#cuisine-input").val();
@@ -36,11 +39,11 @@ $("#search-btn").on("click", function (event) {
     if (cuisine === "") {
       var queryURL =
         "https://developers.zomato.com/api/v2.1/search?entity_id=" +
-        info.entity_id + "&entity_type=" + info.entity_type + "&count=100";
+        info.entity_id + "&entity_type=" + info.entity_type +"&start="+start+ "&count=10";
     } else {
       var queryURL =
         "https://developers.zomato.com/api/v2.1/search?entity_id=" + info.entity_id +
-        "&entity_type=" + info.entity_type + "&q=" + cuisine + "&count=100";
+        "&entity_type=" + info.entity_type + "&q=" + cuisine + "&count=10";
     }
 
     $.ajax({
@@ -50,12 +53,24 @@ $("#search-btn").on("click", function (event) {
         "user-key": "b77bc3b6066b58fd02f4c97a8b61ee93"
       }
     }).then(function (response) {
+      var resultsArr = response.restaurants;
       $("#holder").html("<div id='accordion'>");
-      for (i = 0; i < response.restaurants.length; i++) {
-        $("#accordion").append("<h3>" + response.restaurants[i].restaurant.name + "</h3>\
-          <div>\<p>" + JSON.stringify(response.restaurants[i].restaurant.location) +
-          "<p>\</div>");
-      }
+      for (i = 0; i < resultsArr.length; i++) {
+          var title = resultsArr[i].restaurant.name;
+          var resImage = resultsArr[i].restaurant.thumb;
+          var url = resultsArr[i].restaurant.url;
+          var address = resultsArr[i].restaurant.location.address;
+          var cuisine = resultsArr[i].restaurant.cuisines;
+          var cost = resultsArr[i].restaurant.average_cost_for_two;
+          var rating = resultsArr[i].restaurant.user_rating.aggregate_rating;
+          var ratingCount = resultsArr[i].restaurant.user_rating.votes
+          var menu = resultsArr[i].restaurant.menu_url
+          
+          
+          $("#accordion").append("<h3 class = acordHeader>" + title + "</h3>\<div>" + 
+          "<div class='row bar-container'><div class='col-md-3'><img class='barPhoto'src='" + resImage + "'/></div>" + "<div class = 'col-md-9'><a id=barTitle href ='" +  url + "'target='_blank'>" + title + "</a><br/>" + "Address: " + address + "<br/>" + "Cusine Type: "  + cuisine + "<br/>" + "Estimated price of two: " + cost + "<br/>" + "Rating: "+ rating +"/5 based on " + ratingCount + " votes" +"<br/>" + "<a href ='" +  menu + "'target='_blank'> Click here for the Menu!" + "</a></div></div>" +"</div>")
+      };
+      console.log(resultsArr[0])
       $("#accordion").accordion();
     });
   }
@@ -79,6 +94,7 @@ $("#holder").on("click", ".getCity", function (event) {
 
     $("#cities-input").replaceWith(
       "<p>" + response.location_suggestions[position].title + "</p>"
+      
     );
     $(".city").remove();
     cityInfo = true;
