f63f42270704099889f23ba85b4561caed8bfd3a
ColePresnell <cole.presnell@gmail.com>
2018-05-15 19:41:00 -0500

    fixed next/previous buttons

--- script/drink.js
+++ script/drink.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
     var results = [];
-    var drinks = []
+    var drinks = [];
+    var barSearchInput;
+    var offset;
+    var limit;
     // Initialize Firebase
     var config = {
       apiKey: "AIzaSyB3Q5bof0seihi9umEMn1LSd13Zwr94bmU",
@@ -17,33 +20,41 @@ $(document).ready(function () {
       event.preventDefault();
 
       // Here we grab the text from the input box      
-      var barSearchInput = $("#zip-input").val(); 
+      barSearchInput = $("#zip-input").val(); 
       //attempting to add "next", "previous" button functionality
-      var offset = 0;
-      var limit = 5;
+      offset = 0;
+      limit = 5;
+
+      loadResults();
+      
+    });
 
       $("#drinkNextButton").on("click", function (event) {
         event.preventDefault();
-        offset + 5;
-        limit + 5;
+        offset += 5;
+        limit += 5;
 
-       // function (response);  
+       loadResults();  
     }); 
 
     $("#drinkPreviousButton").on("click", function (event) {
         event.preventDefault();
-        offset - 5;
-        limit - 5;
+        offset -= 5;
+        limit -= 5;
 
-       // $.get(proxyUrl + targetUrl, function (response));  
+       loadResults();  
     }); 
-      
+
+    function loadResults() {
+      $("#barCard").empty();
+      console.log(offset);
+      console.log(limit);
       if ($("#inputGroupSelect01").val() && $("#inputGroupSelect01").val() !== 'none')
         barSearchInput += "&type=" + $("#inputGroupSelect01").val();
       
         firebase.database().ref().push({
           barSearchInput
-     })
+     });
       // Here we construct our URL
       var queryURL = "https://api.barzz.net/api/search?zip=" + barSearchInput + "&offset=" + offset + "&limit=" + limit + "&user_key=8dd53e25d52b2fc98fb6a563b70feaaf";
       console.log(queryURL);
@@ -52,25 +63,26 @@ $(document).ready(function () {
       var targetUrl = queryURL;
 
       $.get(proxyUrl + targetUrl, function (response) {
-        console.log(response);
         var response1 = JSON.parse(response);
-
-        for (var i = offset; i < limit; i++) {
-          console.log(response1.success.results[i]);
-          var barTitle = response1.success.results[i].Name;
-          var barImage = response1.success.results[i].Bar_Image;
-          var barURL = response1.success.results[i].Bar_Url;
-          var barAddress = response1.success.results[i].Address +", "+ response1.success.results[i].City +", "+ response1.success.results[i].State + ", " + response1.success.results[i].Zip + ", Phone: " + response1.success.results[i].Phone;
-          var barWebsite = response1.success.results[i].Bar_Website;
-          var barHours = response1.success.results[i].Hours;
-          var barType = response1.success.results[i].Type;
+        console.log(response1);
+        var resultsArr = response1.success.results;
+        
+        for (var i = 0; i < resultsArr.length; i++) {
+          console.log(resultsArr[i]);
+          var barTitle = resultsArr[i].Name;
+          var barImage = resultsArr[i].Bar_Image;
+          var barURL = resultsArr[i].Bar_Url;
+          var barAddress = resultsArr[i].Address +", "+ resultsArr[i].City +", "+ resultsArr[i].State + ", " + resultsArr[i].Zip + ", Phone: " + resultsArr[i].Phone;
+          var barWebsite = resultsArr[i].Bar_Website;
+          var barHours = resultsArr[i].Hours;
+          var barType = resultsArr[i].Type;
 
           $("#barCard").append(
             "<div class='row bar-container'><div class='col-md-3'><img class='barPhoto'src='" + barImage + "'/></div>" + "<div class = 'col-md-9'><a id=barTitle href ='" +  barURL + "'target='_blank'>" + barTitle + "</a><br/>" + "Address: " + barAddress + "<br/>" + "Website: "  + barWebsite + "<br/>" + "Hours: " + barHours + "<br/>" + "Venue Type: " + barType + "</div></div>")
         };
       });
-    });
-
+    };
+  
 // trying to add "next" and "previous" functionality
 
     firebase.database().ref().on("child_added",function(snapshot){
